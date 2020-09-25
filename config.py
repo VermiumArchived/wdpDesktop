2925ccdabeeeaf86648ddac9f9ba4b0ffe5ecc3a
Vermium Sifell <axel@vermium.se>
2020-09-25 22:29:03 +0200

    Initial commit of WhatsDisPic

---
+++ config.py
@@ -0,0 +1,7 @@
+numGuess = 10 # AI Guesses
+
+fileTypes = ['.png', '.jpg', '.bmp', '.webp', '.jpeg'] # Image Formats
+
+root = './images' # Directory to images root
+
+model = './models/resnet50_weights_tf_dim_ordering_tf_kernels.h5' # Used model - download at https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0
--- main.py
+++ main.py
@@ -1,8 +1,114 @@
-from colorthief import ColorThief
+# -*- coding: utf-8 -*-
+# WhatsDisPic?
 
-color_thief = ColorThief('./images/hund.png')
-# get the dominant color
-dominant_color = color_thief.get_color(quality=1)
-print(dominant_color)
-# build a color palette
-palette = color_thief.get_palette(color_count=6)
+__author__ = "Vermium Sifell, Temerold"
+__copyright__ = "MIT License - (© 2020 Zyner)"
+__version__ = "dev"
+__email__ = "support@vermium.se, support@Temerold.se"
+__status__ = "In development"
+
+
+# ./images/toDo - files to transcode
+# ./images/failed - files that have had failed
+# ./images/completed - files that have successfully completed
+
+
+from imageai.Prediction import ImagePrediction
+from colorthief import ColorThief
+import os
+from PIL import Image, ImageColor
+import shutil
+from time import sleep
+
+from config import * # User configuration - Local file (config.py)
+
+execution_path = os.getcwd()
+
+clear = lambda: os.system('cls')
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath(os.path.join(execution_path, model)) # Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0
+prediction.loadModel()
+
+dominant_color = ""
+pattern = ""
+run = True
+
+yes = ['y', 'yes']
+no = ['n', 'no']
+
+try: os.mkdir(root) except: pass # Create ./images
+
+try: os.mkdir(root + "\\toDo") except: pass # Create ./images/toDo
+
+try: os.mkdir(root + "\\failed") except: pass # Create ./images/failed
+
+try: os.mkdir(root + "\\completed") except: pass # Create ./images/completed
+
+try: os.mkdir(./models) except: pass # Create ./models
+
+for root, dirs, files in os.walk(root, topdown=False):
+    clear()
+    for name in [fi for fi in files if fi.lower().endswith(tuple(fileTypes))]:
+        base, ext = os.path.splitext(name)
+        
+        im = Image.open(root + '\\' + name) # Opening ./images/picture.ext
+        
+        width, height = im.size # Taking the width of that pictures
+        im.close() # Closing the opened image ./images/picture.ext
+        color_thief = ColorThief(root + '\\' + name) # Getting image's color-properties
+        dominant_color = color_thief.get_color(quality = 1) # Getting the image dominant color
+        predictions, probabilities = prediction.predictImage((root + '\\' + name), result_count=numGuess) # AI engine calculates the image in a numGuess count of results
+        
+        print(
+            "Image Name: %s" % str(name) + # Prints name of image
+            "\nDominant Color: %s" % str(dominant_color) + # Prints Dominant color
+            "\nPredictions:" # Print title of Predictions
+        )
+
+        try: # Try making the ./images/picture/
+            os.mkdir(root + "\\completed\\" + base) # Create directory for image ./images/picture/
+        except: pass # Pass, if error/already exists
+
+        ima = Image.new("RGB", (width, height), (dominant_color)) # Creating a image base with dominant color
+
+        ima.save(root + "\\completed\\" + base + "\\" + "dominantColor" + ext) # Saves the dominant color image in ./images/picture/dominant.ext
+
+        ima.close() # Close dominant image
+
+        try: # Try move image from ./images/picture.ext to ./images/picture/original.ext
+            shutil.move((root + "\\toDo\\" + name), (root + "\\completed\\" + base + "\\original" + ext))
+        except: # If error
+            ask_continue = input("Failed at moving file " + name + ", do you want to continue? (no/yes)")
+            if ask_continue in no: exit() # Exit program if answered no
+            elif ask_continue in yes: # Do statement if answered yes
+                try:
+                    shutil.move((root + "\\toDo\\" + name), (root + "\\failed\\" + base + "-failed" + ext)) # Rename ./images/picture.ext to ./images/picture-failed.ext
+                except:
+                    secs = 10 # Timer set for 10 seconds
+                    for sec in range(secs - 1):
+                        clear() # Clear console
+                        print(
+                            "Couldn't rename file to %s-failed%s" % (base, ext) + # Prints Couldn't rename file to picture-failed.ext
+                            "\n Closing program in %s seconds." % secs # Prints closing in x secs
+                        )
+                        secs -= 1 # Remove 1 second from timer
+                        sleep(1) # Wait 1 second
+                    exit() # Exit program
+
+        try: # Try to open a new CSV file
+            out = open((root + "\\completed\\" + base + "\\" + "predictions.csv"), 'w') # Opening a CSV file
+        except: pass # Pass if error / already exists
+        
+        for eachPrediction, eachProbability in zip(predictions, probabilities): # Do the things below for each Prediction
+            print( 
+                "    %s: %s" % ( # Prints Prediction: Probability
+                    str(eachPrediction), # Prediction name
+                    str(round(eachProbability, 1)) # Prediction Percentage
+                ) +
+                "%" # Add a percentage symbol after Prediction: Probability
+            )
+            out.write(str(eachPrediction) + '; ' + str(eachProbability) + '%\n') # Write each prediction to the CSV file
+
+        out.close() # Close .csv file
